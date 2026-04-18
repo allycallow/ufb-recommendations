b0c460e47bb38ad45bf2704fdc1e2b577c5e3fcc
Alex Callow <allycallow@hotmail.com>
2026-04-18 10:38:18 +0100

    feat: init

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from os import getenv
+
+import sentry_sdk
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+
+SENTRY_DSN = getenv("SENTRY_DSN", None)
+STAGE = getenv("STAGE", "local")
+
+sentry_sdk.init(dsn=SENTRY_DSN, send_default_pii=True, environment=STAGE)
+
+app = FastAPI()
+
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "success": False,
+            "message": exc.detail,
+            "statusCode": exc.status_code,
+        },
+    )
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    raise Exception("This is a test exception for Sentry debugging")
---
+++ app/utils/logger.py
@@ -0,0 +1,45 @@
+import json
+import sys
+from os import getenv
+
+from logtail import LogtailHandler  # type: ignore
+from loguru import logger
+
+BETTER_STACK_TOKEN = getenv("BETTER_STACK_TOKEN")
+
+handler = LogtailHandler(
+    source_token=BETTER_STACK_TOKEN,
+    host="https://s2379765.eu-fsn-3.betterstackdata.com",
+)
+
+
+def json_sink(message):
+    record = message.record
+
+    log_dict = {
+        "level": record["level"].name.lower(),
+        "time": record["time"].isoformat(),
+        "msg": record["message"].strip(),  # <-- changed from "formatted" to "message"
+        "name": record["name"],
+        "file": f"{record['file'].name}:{record['line']}",
+    }
+
+    extra = record.get("extra", {})
+    if "extra" in extra and isinstance(extra["extra"], dict):
+        extra = {**extra, **extra.pop("extra")}
+
+    log_dict.update(extra)
+
+    sys.stdout.write(json.dumps(log_dict) + "\n")
+    sys.stdout.flush()
+
+
+logger.remove()
+
+logger.add(
+    sys.stderr, format="{time:MMMM D, YYYY > HH:mm:ss} | {level} | {message} | {extra}"
+)
+
+logger.add(json_sink)
+
+logger.add(handler, format="{message}", backtrace=False, diagnose=False)
